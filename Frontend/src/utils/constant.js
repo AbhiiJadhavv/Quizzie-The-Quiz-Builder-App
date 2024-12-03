d72b37575a18b55b52e0ea13e79daa9907114b9b
Abhishek Jadhav <abhiijadhavv@gmail.com>
2024-12-03 23:30:13 +0530

    added backend link

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const QUIZ_API_END_POINT="http://localhost:8000/api/v1/quiz";
+export const USER_API_END_POINT="https://quizzie-the-quiz-builder-app.onrender.com/api/v1/user";
+export const QUIZ_API_END_POINT="https://quizzie-the-quiz-builder-app.onrender.com/api/v1/quiz";
