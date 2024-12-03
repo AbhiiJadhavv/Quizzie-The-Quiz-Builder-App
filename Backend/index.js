7b9bb7cf317f6d3aceaaa47a3e13074bed2c3d2e
Abhishek Jadhav <abhiijadhavv@gmail.com>
2024-12-03 23:45:35 +0530

    added backend link

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://quizzie-the-quiz-builder-app.vercel.app',
     credentials:true
 }
 
